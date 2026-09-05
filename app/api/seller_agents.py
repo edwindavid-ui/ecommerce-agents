e412a577983e71bfd6b04f996ad297013829259b
Bankole Victor / BLAZELOGIC <101842219+victor2099@users.noreply.github.com>
2026-09-05 01:27:59 +0100

    Add Query import to seller_agents.py

--- app/api/seller_agents.py
+++ app/api/seller_agents.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, HTTPException, status, Depends, Query
 from app.db.mongodb import database
 from app.auth.deps import get_current_user_id
 
